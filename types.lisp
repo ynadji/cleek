b45c0bcc10cdb0138a072038b0be399794933e5e
Yacin Nadji <ynadji@gmail.com>
2025-03-04 13:42:47 -0500

    infer path/log type/fields/types from JSON for common log types

--- types.lisp
+++ types.lisp
@@ -82,3 +82,45 @@
 
 ;; TODO: in order to properly support JSON->Zeek transforms, you'll need to have a map of all
 ;; possible field names to the zeek types, which kinda sucks. does that matter? unclear.
+
+(defparameter *path->fields*
+  '(
+    (:analyzer . (:ts :cause :analyzer_kind :analyzer_name :uid :fuid :id.orig_h :id.orig_p :id.resp_h :id.resp_p :failure_reason :failure_data))
+    (:conn . (:ts :uid :id.orig_h :id.orig_p :id.resp_h :id.resp_p :proto :service :duration :orig_bytes :resp_bytes :conn_state :local_orig :local_resp :missed_bytes :history :orig_pkts :orig_ip_bytes :resp_pkts :resp_ip_bytes :tunnel_parents))
+    (:dns . (:ts :uid :id.orig_h :id.orig_p :id.resp_h :id.resp_p :proto :trans_id :rtt :query :qclass :qclass_name :qtype :qtype_name :rcode :rcode_name :AA :TC :RD :RA :Z :answers :TTLs :rejected))
+    (:files . (:ts :fuid :uid :id.orig_h :id.orig_p :id.resp_h :id.resp_p :source :depth :analyzers :mime_type :filename :duration :local_orig :is_orig :seen_bytes :total_bytes :missing_bytes :overflow_bytes :timedout :parent_fuid :md5 :sha1 :sha256 :extracted :extracted_cutoff :extracted_size))
+    (:http . (:ts :uid :id.orig_h :id.orig_p :id.resp_h :id.resp_p :trans_depth :method :host :uri :referrer :version :user_agent :origin :request_body_len :response_body_len :status_code :status_msg :info_code :info_msg :tags :username :password :proxied :orig_fuids :orig_filenames :orig_mime_types :resp_fuids :resp_filenames :resp_mime_types))
+    (:packet_filter . (:ts :node :filter :init :success :failure_reason))
+    (:quic . (:ts :uid :id.orig_h :id.orig_p :id.resp_h :id.resp_p :version :client_initial_dcid :client_scid :server_scid :server_name :client_protocol :history))
+    (:ssh . (:ts :uid :id.orig_h :id.orig_p :id.resp_h :id.resp_p :version :auth_success :auth_attempts :direction :client :server :cipher_alg :mac_alg :compression_alg :kex_alg :host_key_alg :host_key))
+    (:ssl . (:ts :uid :id.orig_h :id.orig_p :id.resp_h :id.resp_p :version :cipher :curve :server_name :resumed :last_alert :next_protocol :established :ssl_history :cert_chain_fps :client_cert_chain_fps :sni_matches_cert))
+    (:weird . (:ts :uid :id.orig_h :id.orig_p :id.resp_h :id.resp_p :name :addl :notice :peer :source))
+  ))
+
+(defparameter *path->types*
+  '(
+    (:analyzer . (:time :string :string :string :string :string :addr :port :addr :port :string :string))
+    (:conn . (:time :string :addr :port :addr :port :enum :string :interval :count :count :string :bool :bool :count :string :count :count :count :count :set[string]))
+    (:dns . (:time :string :addr :port :addr :port :enum :count :interval :string :count :string :count :string :count :string :bool :bool :bool :bool :count :vector[string] :vector[interval] :bool))
+    (:files . (:time :string :string :addr :port :addr :port :string :count :set[string] :string :string :interval :bool :bool :count :count :count :count :bool :string :string :string :string :string :bool :count))
+    (:http . (:time :string :addr :port :addr :port :count :string :string :string :string :string :string :string :count :count :count :string :count :string :set[enum] :string :string :set[string] :vector[string] :vector[string] :vector[string] :vector[string] :vector[string] :vector[string]))
+    (:packet_filter . (:time :string :string :bool :bool :string))
+    (:quic . (:time :string :addr :port :addr :port :string :string :string :string :string :string :string))
+    (:ssh . (:time :string :addr :port :addr :port :count :bool :count :enum :string :string :string :string :string :string :string :string))
+    (:ssl . (:time :string :addr :port :addr :port :string :string :string :string :bool :string :string :bool :string :vector[string] :vector[string] :bool))
+    (:weird . (:time :string :addr :port :addr :port :string :string :bool :string :string))
+  ))
+
+(defun infer-log-path-fields-types (zeek-log)
+  (unless (zeek-path zeek-log)
+    (ensure-map zeek-log)
+    (ensure-fields zeek-log)
+    ;; LOOP across known fields find first full coverage match, update
+    ;; fields and types.
+    (loop for (path . fields) in *path->fields*
+          for (nil . types) in *path->types*
+          unless (set-difference (zeek-fields zeek-log) fields)
+            do (setf (zeek-path zeek-log) path
+                     (zeek-fields zeek-log) fields
+                     (zeek-types zeek-log) types))
+    zeek-log))
