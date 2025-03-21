f00c3b00486f3504a8c69a97d0ad60d19e8ab957
Yacin Nadji <ynadji@gmail.com>
2025-03-21 07:14:13 -0400

    more filter comments

--- filters.lisp
+++ filters.lisp
@@ -17,6 +17,11 @@
 ;;; only makes sense for strings, no?
 ;; string=, s=
 ;;; you could shadow = from CL-USER and do the same kind of dispatch.
+;; matches from a file
+;;; maybe something like:
+;;; (na:contains? (f "filename" :str/:ip/:domain) #I(:o_h))
+;;; would load the ips/nets in "filename" and check against contains? you'd want to #. on it so it gets evaluated
+;;; immediately and stored as the #<IP-SET> object (or LIST or w/e it is).
 
 ;; this might be faster as a macro, since you (might?) get the benefits of the
 ;; scanner caching for free.
