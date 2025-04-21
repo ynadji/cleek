297b8f64343c1d099eba38dac07ada28dd96d099
Yacin Nadji <ynadji@gmail.com>
2025-04-20 21:57:07 -0400

    add some more perf tests

--- perf.lisp
+++ perf.lisp
@@ -74,6 +74,10 @@
 (require :sb-sprof)
 
 (defparameter *big-conn* (asdf:system-relative-pathname "cleek" "data/test-input/homenet-uncompressed.zeek.log"))
+(defparameter *big-conn-json* (asdf:system-relative-pathname "cleek" "data/test-input/homenet-uncompressed.json.log"))
+
+(let ((*common-filters-and-mutators-path* (asdf:system-relative-pathname "cleek" "common-filters-and-mutators.lisp")))
+  (init-common-filters-and-mutators))
 
 (defparameter *filters*
   `(("nil" nil nil)
@@ -82,25 +86,46 @@
     ("many-matches-str-contains" nil "(c? \"tcp\" LINE)")
     ("many-matches-regex" nil "(~ \"tcp\" LINE)")
 
-    ("one-field-s=" nil "(s= @o_h \"8.8.8.8\")")))
+    ("one-field-s=" nil "(s= @o_h \"8.8.8.8\")")
+    ("productive-tcp" nil "(and (s= @proto \"tcp\") productive?)")
+    ("productive-tcp-bignatius-orig" nil "(and (s= @proto \"tcp\")
+                                               productive?
+                                               (c? @@id.orig_h_name.vals \"bignatius.nadji.us\"))")
+    ("productive-tcp-big-orig-more" nil "(and (s= @proto \"tcp\")
+                                               productive?
+                                               (c? @@id.orig_h_name.vals \"bignatius.nadji.us\")
+                                               (not @@local_orig)
+                                               (= 1 (length @@id.resp_h_name.vals))
+                                               (not (c? @@id.resp_h_name.vals \"-\")))")
+
+    ("set-lengths-prod-tcp-filter" "(setf @resp_names_length (length @@id.resp_h_name.vals)
+                                          @orig_names_length (length @@id.orig_h_name.vals))"
+                                   "(and (s= @proto \"tcp\")
+                                         productive?
+                                         (c? @@id.orig_h_name.vals \"bignatius.nadji.us\")
+                                         (not @@local_orig)
+                                         (not (minusp @orig_names_length))
+                                         (= 1 @resp_names_length)
+                                         (not (c? @@id.resp_h_name.vals \"-\")))")))
 
 
-(defun run-perf-test (name mutator-expr filter-expr csv-stream &optional profile? (n 3))
+(defun run-perf-test (input-log output-format name mutator-expr filter-expr csv-stream &optional profile? (n 3))
   (format t "# Running test ~a (profiling ~a)~%" name profile?)
   (if profile?
       (sb-sprof:with-profiling (:report :graph :sample-interval 0.001)
-        (cat-logs-string #P"perf.log" :zeek mutator-expr filter-expr *big-conn*))
+        (cat-logs-string #P"perf.log" output-format mutator-expr filter-expr input-log))
       ;; i really need to output this to a CSV so i can easily compare before and afters.
       (progn (format csv-stream "~a," name)
              (my-time csv-stream
                       (dotimes (n n)
-                        (cat-logs-string #P"perf.log" :zeek mutator-expr filter-expr *big-conn*))))))
+                        (cat-logs-string #P"perf.log" output-format mutator-expr filter-expr input-log))))))
 
 (defun run-all-perf-tests (output-path)
   (with-open-file (stream output-path :direction :output :if-exists :supersede)
     (format stream "test-name,real-time,user-time,system-time,gc-real-time,non-gc-real-time,bytes-consed~%")
     (loop for (name mutator-expr filter-expr) in *filters*
-          do (run-perf-test name mutator-expr filter-expr stream)
-             (sb-ext:gc :full t)
-             (run-perf-test name mutator-expr filter-expr stream t)
-             (sb-ext:gc :full t))))
+          do (loop for input-log in (list *big-conn* *big-conn-json*) for type in (list :zeek :json)
+                   do (run-perf-test input-log type (format nil "~a-~(~a~)" name type) mutator-expr filter-expr stream)
+                      (sb-ext:gc :full t)
+                      (run-perf-test input-log type (format nil "~a-~(~a~)" name type) mutator-expr filter-expr stream t)
+                      (sb-ext:gc :full t)))))
